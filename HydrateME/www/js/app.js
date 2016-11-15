e765c93dd899aab53983ca872cbd8eb05ca4b1b6
Yeam Wee Kian <weekian.yeam@gmail.com>
2016-11-15 20:18:41 +0800

    - Update to app

--- HydrateME/www/js/app.js
+++ HydrateME/www/js/app.js
@@ -5,12 +5,12 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('app', ['ionic', 'app.controllers', 'app.routes', 'app.directives','app.services',])
+angular.module('app', ['ionic', 'app.controllers', 'app.routes', 'app.directives','app.services','angularMoment'])
 
 .config(function($ionicConfigProvider){
-  
     $ionicConfigProvider.backButton.text('').previousTitleText(false);
-  
+    $ionicConfigProvider.tabs.position("bottom");
+
 })
 
 .run(function($ionicPlatform) {
