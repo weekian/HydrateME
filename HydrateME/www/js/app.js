47bd4d32129c5bea523e81d138901677ac850b04
Yeam Wee Kian <weekian.yeam@gmail.com>
2016-11-19 23:43:38 +0800

    - inclusion of charts

--- HydrateME/www/js/app.js
+++ HydrateME/www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('app', ['ionic', 'app.controllers', 'app.routes', 'app.directives','app.services','angularMoment', 'ngCordova'])
+angular.module('app', ['ionic', 'app.controllers', 'app.routes', 'app.directives','app.services','angularMoment', 'ngCordova','chart.js'])
 
 .config(function($ionicConfigProvider){
     $ionicConfigProvider.backButton.text('').previousTitleText(false);
